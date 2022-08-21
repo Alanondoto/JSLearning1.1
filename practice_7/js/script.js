0aba0503168955d71ac2825347432e67b2b3323d
alanondoto <alexgusevtoo@gmail.com>
2022-08-21 12:38:00 +0300

    17th part practiсe

--- practice_7/js/script.js
+++ practice_7/js/script.js
@@ -40,7 +40,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
     // Timer
 
-    const deadline = '2022-08-20';
+    const deadline = '2022-08-22';
 
     function getTimeRemaining(endtime) {
         const t = Date.parse(endtime) - Date.parse(new Date()),
@@ -84,6 +84,60 @@ window.addEventListener('DOMContentLoaded', () => {
             }
         }
     }
-    setClock('.timer', deadline); 
+    setClock('.timer', deadline);
+    
+    // Modal
+
+    // const modal = document.querySelector('.modal'),
+    //       modalBtn = document.querySelectorAll('[data-modal]'),
+    //       modalCross = modal.querySelector('[data-close]');
+
+    // function showModal() {
+    //     modal.style.display = 'block';
+    // }
+
+    // function closeModal() {
+    //     modal.style.display = 'none';
+    // }
+
+    // modalBtn.forEach(item => {
+    //     item.addEventListener('click', showModal);
+    // });
+
+    // modalCross.addEventListener('click', closeModal);
+
+    const modalTrigger = document.querySelectorAll('[data-modal]'),
+          modal = document.querySelector('.modal'),
+          modalCloseBtn = modal.querySelector('[data-close]');
+
+    modalTrigger.forEach(btn => {
+            btn.addEventListener('click', () => {
+            modal.classList.add('show');
+            modal.classList.remove('hide');
+            // modal.classList.toggle('show');
+            document.body.style.overflow = 'hidden';
+        });
+    });
+
+    function closeModal() {
+        modal.classList.add('hide');
+        modal.classList.remove('show');
+        // modal.classList.toggle('show');
+        document.body.style.overflow = '';    
+    }
+        
+    modalCloseBtn.addEventListener('click', closeModal);
+
+    modal.addEventListener('click', (e) => {
+        if (e.target === modal) {
+            closeModal(); 
+        }
+    });
+
+    document.addEventListener('keydown', (e) => {
+        if (e.code === 'Escape' && modal.classList.contains('show')) {
+            closeModal();
+        }
+    });
 });
 
