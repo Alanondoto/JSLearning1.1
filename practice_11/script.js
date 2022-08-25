e4ee5d2c76b3fc4a04548347417c4c64a3bcd1fb
alanondoto <alexgusevtoo@gmail.com>
2022-08-25 16:16:05 +0300

    19th part practiсe

---
+++ practice_11/script.js
@@ -0,0 +1,25 @@
+'use strict';
+
+// const num = new Number(3);
+// console.log(num);
+
+function User(name, id) {
+    this.name = name;
+    this.id = id;
+    this.human = true;
+    this.hello = function() {
+        console.log(`Hello ${this.name}`);
+    };
+}
+
+User.prototype.exit = function() {
+    console.log(`Пользователь ${this.name} ушел`);
+};
+
+const ivan = new User('Ivan', 28);
+const alex = new User('Alex', 21);
+
+ivan.hello();
+alex.hello();
+
+console.log(ivan, alex);
