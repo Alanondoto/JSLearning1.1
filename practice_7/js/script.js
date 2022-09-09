72fad9e6bb79cf18a2126e2e84449b1f19ab63c1
Alanondoto <alexgusevtoo@gmail.com>
2022-09-09 08:41:09 +0300

    33th part practiсe(axios)

--- practice_7/js/script.js
+++ practice_7/js/script.js
@@ -212,12 +212,19 @@ window.addEventListener('DOMContentLoaded', () => {
         return await res.json();
     };
 
-    getResource('http://localhost:3000/menu')
+    axios.get('http://localhost:3000/menu')
         .then(data => {
-            data.forEach(({img, altimg, title, descr, price}) => {
+            data.data.forEach(({img, altimg, title, descr, price}) => {
                 new MenuCard(img, altimg, title, descr, price, '.menu .container ').render();
             }); 
         });
+
+    // getResource('http://localhost:3000/menu')
+        // .then(data => {
+        //     data.forEach(({img, altimg, title, descr, price}) => {
+        //         new MenuCard(img, altimg, title, descr, price, '.menu .container ').render();
+        //     }); 
+    //     });
 
     // getResource('http://localhost:3000/menu')
     //     .then(data => createCard(data));
@@ -328,9 +335,9 @@ window.addEventListener('DOMContentLoaded', () => {
     // .then(response => response.json())
     // .then(json => console.log(json));
 
-    fetch('http://localhost:3000/menu')
-        .then(data => data.json())
-        .then(res => console.log(res));
+    // fetch('http://localhost:3000/menu')
+    //     .then(data => data.json())
+    //     .then(res => console.log(res));
 
 });
 
