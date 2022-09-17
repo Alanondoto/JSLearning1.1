12915ffb992f80ae1686c84c4f80eef64e868bfe
Alanondoto <alexgusevtoo@gmail.com>
2022-09-17 16:47:10 +0300

    48th part practiсe(requestAnimationFrame)

--- practice_8/script.js
+++ practice_8/script.js
@@ -1,24 +1,26 @@
-const btn = document.querySelector('.btn');
-let timerId,
-    i = 0;
+const btn = document.querySelector('.btn'), 
+      elem = document.querySelector('.box');
+let pos = 0;
+// let timerId,
+//     i = 0;
 
-function myAnimation() {
-    const elem = document.querySelector('.box');
-    let pos = 0;
+// function myAnimation() {
+//     const elem = document.querySelector('.box');
+//     let pos = 0;
 
-    const id = setInterval(frame, 10);
-    function frame() {
-        if (pos == 300) {
-            clearInterval();
-        } else {
-            pos++;
-            elem.style.top = pos + 'px';
-            elem.style.left = pos + 'px';
-        }
-    }
-}    
+//     const id = setInterval(frame, 10);
+//     function frame() {
+//         if (pos == 300) {
+//             clearInterval();
+//         } else {
+//             pos++;
+//             elem.style.top = pos + 'px';
+//             elem.style.left = pos + 'px';
+//         }
+//     }
+// }    
 
-btn.addEventListener('click', myAnimation);
+// btn.addEventListener('click', myAnimation);
 
 // function logger() {
 //     if (i === 3) {
@@ -32,3 +34,19 @@ btn.addEventListener('click', myAnimation);
 //     console.log('Hello');
 //     id = setTimeout(log, 500);
 // }, 500);
+
+function myAnimation() {
+    pos++;
+    elem.style.top = pos + 'px';
+    elem.style.left = pos + 'px';
+    if (pos < 300) {
+        requestAnimationFrame(myAnimation);
+    } else {
+        pos = 0;
+    }
+}
+
+btn.addEventListener('click', () => requestAnimationFrame(myAnimation));
+
+let id = requestAnimationFrame(myAnimation);
+cancelAnimationFrame(id);
